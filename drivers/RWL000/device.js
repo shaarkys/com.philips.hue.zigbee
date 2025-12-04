34780d73c21ac8497a5e0739d6b6eeb2af60bd53
shaarkys <bretislav.kubesa@gmail.com>
2025-12-04 13:27:07 +0100

    Battery reporting for HUE Dimmer

--- drivers/RWL000/device.js
+++ drivers/RWL000/device.js
@@ -1,53 +1,94 @@
 'use strict';
 
 const { ZigBeeDevice } = require('homey-zigbeedriver');
-const { CLUSTER } = require('zigbee-clusters');
+const { Cluster, CLUSTER } = require('zigbee-clusters');
 const OnOffBoundCluster = require('../../lib/OnOffBoundCluster');
 const LevelControlBoundCluster = require('../../lib/LevelControlBoundCluster');
+const HueSpecificBasicCluster = require('../../lib/HueSpecificBasicCluster');
+
+Cluster.addCluster(HueSpecificBasicCluster);
 
 class DimmerSwitch extends ZigBeeDevice {
 
 async onNodeInit({ zclNode }) {
 
   this.printNode();
-    
-    // Buttons
-    zclNode.endpoints[1].bind(CLUSTER.ON_OFF.NAME, new OnOffBoundCluster({
-      onSetOn: this._onCommandParser.bind(this),
-      onSetOff: this._offCommandParser.bind(this),
-      offWithEffect: this._offCommandParser.bind(this)
-    }));
 
-    zclNode.endpoints[1].bind(CLUSTER.LEVEL_CONTROL.NAME, new LevelControlBoundCluster({
-      onStep: this._stepCommandParser.bind(this),
-      onStepWithOnOff: this._stepCommandParser.bind(this),
-      onStop: this._stopCommandParser.bind(this),
-      onStopWithOnOff: this._stopCommandParser.bind(this),
-    }));
+  const batteryEndpointId = 2;
 
-    this._switchOnTriggerDevice = this.homey.flow.getDeviceTriggerCard('RWL000_on');
-    this._switchOffTriggerDevice = this.homey.flow.getDeviceTriggerCard('RWL000_off');
-    this._switchDimTriggerDevice = this.homey.flow.getDeviceTriggerCard('RWL000_dim')
-      .registerRunListener(async (args, state) => {
-        return (null, args.action === state.action);
-      });
+  // Buttons
+  zclNode.endpoints[1].bind(CLUSTER.ON_OFF.NAME, new OnOffBoundCluster({
+    onSetOn: this._onCommandParser.bind(this),
+    onSetOff: this._offCommandParser.bind(this),
+    offWithEffect: this._offCommandParser.bind(this)
+  }));
 
-		// alarm_battery
-		if (this.hasCapability('alarm_battery')) {				
-      this.batteryThreshold = 20;
-			this.registerCapability('alarm_battery', CLUSTER.POWER_CONFIGURATION, {
-				getOpts: {
-				},
-				reportOpts: {
-					configureAttributeReporting: {
-						minInterval: 0, // No minimum reporting interval
-						maxInterval: 60000, // Maximally every ~16 hours
-						minChange: 10, // Report when value changed by 10
-					},
-				},
-      });
-		}
+  zclNode.endpoints[1].bind(CLUSTER.LEVEL_CONTROL.NAME, new LevelControlBoundCluster({
+    onStep: this._stepCommandParser.bind(this),
+    onStepWithOnOff: this._stepCommandParser.bind(this),
+    onStop: this._stopCommandParser.bind(this),
+    onStopWithOnOff: this._stopCommandParser.bind(this),
+  }));
 
+  this._switchOnTriggerDevice = this.homey.flow.getDeviceTriggerCard('RWL000_on');
+  this._switchOffTriggerDevice = this.homey.flow.getDeviceTriggerCard('RWL000_off');
+  this._switchDimTriggerDevice = this.homey.flow.getDeviceTriggerCard('RWL000_dim')
+    .registerRunListener(async (args, state) => {
+      return (null, args.action === state.action);
+    });
+
+  await this._configureBattery(zclNode, batteryEndpointId);
+
+  }
+
+  async _configureBattery(zclNode, endpointId) {
+    const isFirstInit = this.isFirstInit();
+    const endpoint = zclNode.endpoints[endpointId];
+    if (!endpoint || !endpoint.clusters[CLUSTER.POWER_CONFIGURATION.NAME]) {
+      this.error(`Power configuration cluster not found on endpoint ${endpointId}`);
+      return;
+    }
+
+    if (isFirstInit && endpoint.clusters.HueSpecificBasicCluster) {
+      try {
+        await endpoint.clusters.HueSpecificBasicCluster.writeAttributes(
+          { philips: 0x000b },
+          { manufacturerCode: 0x100b, disableDefaultResponse: true },
+        );
+      } catch (err) {
+        this.error('Error writing Hue specific Basic attribute (0x0031)', err);
+      }
+    }
+
+    if (!this.hasCapability('measure_battery')) {
+      await this.addCapability('measure_battery');
+    }
+
+    const batteryReportOpts = {
+      configureAttributeReporting: {
+        minInterval: 0,
+        maxInterval: 21600,
+        minChange: 1,
+      },
+    };
+
+    this.registerCapability('measure_battery', CLUSTER.POWER_CONFIGURATION, {
+      endpoint: endpointId,
+      getOpts: {
+        getOnStart: true,
+        getOnOnline: true,
+      },
+      reportOpts: batteryReportOpts,
+    });
+
+    this.batteryThreshold = 20;
+    this.registerCapability('alarm_battery', CLUSTER.POWER_CONFIGURATION, {
+      endpoint: endpointId,
+      getOpts: {
+        getOnStart: true,
+        getOnOnline: true,
+      },
+    });
   }
 
   _onCommandParser() {
